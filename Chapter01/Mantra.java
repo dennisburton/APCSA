545b394ae612581f6cddf147439577461f0db71e
Dennis Burton <dburton@careevolution.com>
2019-08-25 15:05:47 -0400

    Add Excercise 1.7

---
+++ Chapter01/Mantra.java
@@ -0,0 +1,12 @@
+public class Mantra {
+   public static void main( String [] args ){
+      printGatekeeperStatement();
+      System.out.println("");
+      printGatekeeperStatement();
+   }
+   
+   public static void printGatekeeperStatement(){
+      System.out.println("There's one thing every coder must understand:");
+      System.out.println("The System.out.println command.");
+   }
+}
