0c3976e5c5330e685730f80e202e5c65da5ab2a5
Dennis Burton <dburton@careevolution.com>
2019-08-25 14:53:50 -0400

    Add Exercise 1.4

---
+++ Chapter01/Difference.java
@@ -0,0 +1,8 @@
+public class Difference {
+   public static void main( String [] args ){
+      System.out.println("What is the difference between");
+      System.out.println("and ' and a \"? or between a \" and a \\\"?");
+      System.out.println("One is what we see when we're typing our program.");
+      System.out.println("The other is what appears on the \"console.\"");
+   }
+}
