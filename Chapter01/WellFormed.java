261d0d265954079d75973dec1acc8ac4b5d6b064
Dennis Burton <dburton@careevolution.com>
2019-08-25 14:47:44 -0400

    Add Exercise 1.3

---
+++ Chapter01/WellFormed.java
@@ -0,0 +1,16 @@
+public class WellFormed {
+   public static void main( String [] args ){
+      System.out.println("A well-formed Java program has");
+      System.out.println("a main method with { and }");
+      System.out.println("braces.\n"); // Version with escaped newline
+      /* Version with empty println for new line instead of escape character
+      System.out.println("braces.")
+      System.out.println("");
+      */
+      System.out.println("A System.out.println statement");
+      System.out.println("has ( and ) and usually a");
+      System.out.println("String that starts and ends");
+      System.out.println("with a \" character.");
+      System.out.println("(But we type \\\" instead!)");
+   }
+}
